7c94c5fa5113198d729ca98abfb919654a5d6d02
Calvin McDowell <calvinmac40@hotmail.com>
2019-07-04 23:21:11 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,3 +1,3 @@
 first_number = expect(first_number).to be_an(Integer).or be_a(Float)
 second_number =  expect(second_number).not_to equal(0)
-sum = 
+sum = expect(sum).to eq(first_number+second_number)
